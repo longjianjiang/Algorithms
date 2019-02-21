649dae5b891185afa22cd51df0eb0b4e7f428983
longjianjiang <616393956@qq.com>
2019-02-21 16:04:11 +0800

    add CMakeLists.txt

---
+++ main.cpp
@@ -0,0 +1,16 @@
+
+
+
+#include <iostream>
+
+#include "data_structures/binary_search_tree.h"
+#include "data_structures/red_black_bst.h"
+
+using namespace jiang;
+
+int main() {
+  std::cout << "Testing binary search tree<int>" << std::endl;
+
+  std::cout << "Testing red black bst<int>" << std::endl;
+
+}
